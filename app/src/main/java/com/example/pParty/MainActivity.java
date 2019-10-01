22748f545944a6ac65a6835b559fa1a852d2ddc3
Illia Nechesa <nechesaillya@yahoo.es>
2019-10-01 14:28:54 +0200

    User registration and login implemented successfully
    Signed-off-by: Illia Nechesa <nechesaillya@yahoo.es>

--- app/src/main/java/com/example/pParty/MainActivity.java
+++ app/src/main/java/com/example/pParty/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.pParty;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -34,14 +40,23 @@ public class MainActivity extends AppCompatActivity
 
 
         // TODO Aqui hay que meter un if de si la sesion esta iniciada o no
-        if(true) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            navigationView.getMenu().findItem(R.id.nav_login).setVisible(false);
+            navigationView.getMenu().findItem(R.id.nav_signup).setVisible(false);
+            String usermail = user.getEmail();
+            String username = user.getEmail();
+            int index = username.indexOf('@');
+            username = username.substring(0,index);
+            TextView usermail_text = navigationView.getHeaderView(0).findViewById(R.id.usermail);
+            TextView username_text = navigationView.getHeaderView(0).findViewById(R.id.username);
+            usermail_text.setText(usermail);
+            username_text.setText(username);
+        } else {
             navigationView.getHeaderView(0).findViewById(R.id.usermail).setVisibility(View.GONE);
             navigationView.getHeaderView(0).findViewById(R.id.username).setVisibility(View.GONE);
             navigationView.getMenu().findItem(R.id.nav_logout).setVisible(false);
             navigationView.getMenu().findItem(R.id.nav_myprofile).setVisible(false);
-        }else{
-            navigationView.getMenu().findItem(R.id.nav_login).setVisible(false);
-            navigationView.getMenu().findItem(R.id.nav_signup).setVisible(false);
         }
     }
 
@@ -90,13 +105,15 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_trends) {
 
         } else if (id == R.id.nav_login) {
-
+            Intent loginIntent = new Intent(this,LoginActivity.class);
+            startActivity(loginIntent);
         } else if (id == R.id.nav_logout) {
 
         } else if (id == R.id.nav_myprofile) {
 
         }else if (id == R.id.nav_signup) {
-
+            Intent signupIntent = new Intent(this,SignUpActivity.class);
+            startActivity(signupIntent);
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
---
+++ app/src/main/java/com/example/pParty/SignUpActivity.java
@@ -0,0 +1,84 @@
+package com.example.pParty;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    EditText emailId, password, password2;
+    Button btnSignUp;
+    TextView tvSignIn;
+    FirebaseAuth mFirebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.signup_email);
+        password = findViewById(R.id.signup_password);
+        password2 = findViewById(R.id.signup_password_2);
+        btnSignUp = findViewById(R.id.signup_btn);
+        tvSignIn = findViewById(R.id.signin_signup);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailId.getText().toString();
+                String pwd = password.getText().toString();
+                if(email.isEmpty()){
+                    emailId.setError("Please enter email id");
+                    emailId.requestFocus();
+                }
+                else  if(pwd.isEmpty()){
+                    password.setError("Please enter your password");
+                    password.requestFocus();
+                }
+                else  if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(SignUpActivity.this,R.string.empty_fields,Toast.LENGTH_SHORT).show();
+                }
+                else if (!password.getText().toString().equals(password2.getText().toString())){
+                    Toast.makeText(SignUpActivity.this,R.string.different_passwords, Toast.LENGTH_SHORT).show();
+                }
+                else  if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(SignUpActivity.this,R.string.signup_unsuccessful,Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(SignUpActivity.this,MainActivity.class));
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(SignUpActivity.this,R.string.error_ocurred,Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SignUpActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
